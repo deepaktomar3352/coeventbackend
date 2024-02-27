3b469508622a41fd5b2d6849a4941700f54470c6
Deepak Singh Tomar <112108549+deepaktomar3352@users.noreply.github.com>
2024-02-27 10:33:18 +0530

    backend updated

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -151,5 +151,105 @@ router.post('/updated_home_title', function (req, res) {
 
 });
 
+router.get('/showsponsor', function (req, res) {
+  pool.query("select * from sponsor", function (error, result,) {
+
+    if (error) {
+      console.log(error)
+      res.status(500).json({ status: false, message: 'Server error....' })
+    }
+    else {
+      console.log(result)
+      res.status(200).json({ status: true, data: result, message: 'Fetch Sponsor Succesfully' })
+
+    }
+  })
+});
+
+router.get('/showsponsorimg', function (req, res) {
+  pool.query("select * from sponsor", function (error, result) {
+
+    if (error) {
+      console.log(error)
+      res.status(500).json({ status: false, message: 'Server error....' })
+    }
+    else {
+      console.log(result)
+      res.status(200).json({ status: true, data: result, message: 'Fetch Sponsor Succesfully' })
+    }
+
+  })
+
+});
+
+router.post('/sponsorupdate', function (req, res) {
+  pool.query("update sponsor set firstname=?,lastname=?,emailaddress=?,mobilenumber=?,company=?,comment=?,category=?,updatedate=?,updatedby=? where sponsorid=? ", [req.body.fname, req.body.lname, req.body.emailaddress, req.body.mobilenumber, req.body.companyname, req.body.comment, req.body.category, req.body.updatedat, req.body.submitedby, req.body.sponsorid], function (error, result) {
+
+    if (error) {
+      console.log(error)
+      res.status(500).json({ status: false, message: " server error" })
+    }
+    else {
+      console.log(result)
+      res.status(200).json({ status: true, data: result[0], message: " updated succesfully" })
+    }
+  })
+});
+
+
+router.post('/sponsorimgupdate', upload.single('logo'), function (req, res) {
+
+  pool.query("update sponsor set companylogo=? where sponsorid=?", [req.file.originalname, req.body.sponsorid], function (error, result) {
+
+    if (error) {
+      console.log(error)
+      res.status(500).json({ status: false, message: " server error" })
+    }
+    else {
+      console.log(result)
+      res.status(200).json({ status: true, message: " updated succesfully" })
+    }
+  })
+});
+
+router.post('/delete_sponsor_detail', (req, res) => {
+
+  pool.query("delete from sponsor  where sponsorid=?", [req.body.sponsorid], function (error, result) {
+
+    if (error) {
+      console.log(error)
+      res.status(500).json({ status: false, message: " server error" })
+    }
+    else {
+      console.log(result)
+      res.status(200).json({ status: true, data: result[0], message: " Deleted  succesfully" })
+    }
+
+  })
+
+
+});
+
+
+router.get("/sponsordata", function (req, res) {
+  res.render('sponsor', { message: "" });
+})
+
+router.post('/sponsorsubmit', upload.single('logo'), function (req, res, next) {
+  pool.query("insert into sponsor( firstname, lastname, emailaddress, mobilenumber, company, comment, companylogo, category, submitedat, updatedate, updatedby)values(?,?,?,?,?,?,?,?,?,?,?)", [req.body.fname, req.body.lname, req.body.emailaddress, req.body.mobilenumber, req.body.companyname, req.body.comment, req.file.originalname, req.body.category, req.body.submitedat, req.body.updatedat, req.body.submitedby], function (error, result) {
+
+    if (error) {
+      console.log(error)
+      res.status(500).json({ status: false, message: 'Server error....' })
+    }
+    else {
+      console.log(result)
+      res.status(200).json({ status: true, message: 'Submited Succesfully' })
+    }
+
+
+  });
+});
+
 
 module.exports = router;
